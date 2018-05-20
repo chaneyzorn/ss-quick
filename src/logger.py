7ea073734e8f53353591b18b27013973778b7023
chaney <chaneyzorn@gmail.com>
2018-05-20 15:12:07 +0800

    fix log format

--- src/logger.py
+++ src/logger.py
@@ -3,6 +3,6 @@ from logging import getLogger, INFO, basicConfig
 basicConfig(
     level=INFO,
     format='%(asctime)s %(levelname)s: %(message)s',
-    datefmt='%M-%d %H:%M:%S'
+    datefmt='%m-%d %H:%M:%S'
 )
 ss_log = getLogger("ss-up")
